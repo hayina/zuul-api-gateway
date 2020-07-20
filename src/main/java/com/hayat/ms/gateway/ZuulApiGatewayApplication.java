b5ad5b3d5c6000ec198cdc548e56c90f5a346e15
youssef <youssef.lechqar@gmail.com>
2020-07-20 10:02:21 +0100

    setting up basic configuration

--- src/main/java/com/hayat/ms/gateway/ZuulApiGatewayApplication.java
+++ src/main/java/com/hayat/ms/gateway/ZuulApiGatewayApplication.java
@@ -2,8 +2,12 @@ package com.hayat.ms.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableZuulProxy
 public class ZuulApiGatewayApplication {
 
     public static void main(String[] args) {
